d99cdc0ce86923625639aca8fdd8caacfe5d4d4d
Alaa  Jamal Abdalqader <93215532+eylem2002@users.noreply.github.com>
2023-11-28 19:53:34 +0300

    final_signIn

--- lib/view/screen/signin.dart
+++ lib/view/screen/signin.dart
@@ -11,7 +11,7 @@ class LogIN extends StatefulWidget {
   State<LogIN> createState() => _LogINState();
 }
 
-// AppImageAsses appImageAsses= AppImageAsses();
+
 
 class _LogINState extends State<LogIN> {
 
@@ -29,149 +29,163 @@ class _LogINState extends State<LogIN> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffEAEBEF),
-      body: Container(
-        padding: const EdgeInsets.only(top: 75),
-        child: Form(
-            key: _formField,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 210,
-                    height: 210,
-                  child: Image.asset(ImageAsses.logo),
-                  ),
-                  Container(
-                    child: Container(
-                      margin: EdgeInsets.only(
-                          right: MediaQuery.of(context).size.width * 0.70,
-                          bottom: MediaQuery.of(context).size.height * 0.01),
-                      child: const Text(
-                        "Sign In",
-                        style: TextStyle(
-                            fontSize: 25,
-                            color: Color(0xff263238),
-                            fontWeight: FontWeight.w600,
-                            fontFamily: 'Kadwa'),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 20,
-                  ),
-                  
-                   TextFormFieldWidget(height:0.068,width: 0.9,passToggle: false,passController: _controller,label_text: 'Email',validat:  (value) {
-                      if (value!.isEmpty) {
-                        return "Enter Email";
-                      }
-                      bool emailValid = RegExp(
-                              r"^[a-zA-Z0-9.a-zA-Z0-9.1#$&'*+-/=?^_ {|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(value);
-                      if (!emailValid) {
-                        return "Enter valid Email";
-                      }
-                      return null;
-                    }
-                  ,),
-
-                  Container(
-                    height: 30,
-                  ),
-
-
-                  TextFormFieldWidget(height:0.068,width: 0.9,passToggle: true,passController: _controller_pass,label_text: 'password',validat: 
-                    (value) {
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(top: 75),
+          child: Form(
+              key: _formField,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                //  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
                    
-                     if(value!.isEmpty){
-                      return "Enter Password";
-                     }else if(value.length<6){
-                                            return "Password length should be more than 6 characters";
-
-                     }else{
-                      return null;
-                     }
-                     
-                      
-
-                    }
-                  ,)
-                ,
-                SizedBox(height: widthNHeight0(context, 0)*0.07,),
-                  Container(
-                    padding: const EdgeInsets.only(left: 30, right: 30),
-                    height: 60,
-                    child: InkWell(
-                      onTap: () {
-                        if (_formField.currentState!.validate()) {
-                          print("Success");
-                          emailController.clear();
-                          passController.clear();
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (_) => const Home_page()));
+                  Column(
+                    children: [
+                       Container(
+                      width: 210,
+                      height: 210,
+                    child: Image.asset(ImageAsses.logo),
+                    ),
+                   
+                    Container(
+                        margin: EdgeInsets.only(
+                            right: MediaQuery.of(context).size.width * 0.70,
+                            bottom: MediaQuery.of(context).size.height * 0.01),
+                        child: const Text(
+                          "Sign In",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color(0xff263238),
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Kadwa'),
+                        ),
+                      ),
+                    
+                    Container(
+                      height: 20,
+                    ),
+                    
+                     TextFormFieldWidget(passToggle: false,passController: _controller,label_text: 'Email',validat:  (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Email";
+                           
+                          
+                          
                         }
-                      },
-                      child: Container(
-                       // height: MediaQuery.of(context).size.width * .40,
-                      width: widthNHeight0(context, 0)*0.7, //////////////////edit the width
-                        decoration: BoxDecoration(
-                            color: const Color(0xff6482C4),
-                            borderRadius: BorderRadius.circular(5)),
-                        child: const Center(
-                          child: Text(
-                            "Sign In",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontFamily: 'Kadwa',
-                                fontWeight: FontWeight.w600),
+                        bool emailValid = RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.1#$&'*+-/=?^_ {|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value);
+                        if (!emailValid) {
+                          return "Enter valid Email";
+                        }
+                            return null;
+                      }
+                    ,str: " Enter Email"),
+        
+                    Container(
+                      height: 30,
+                    ),
+        
+        
+                    TextFormFieldWidget(passToggle: true,passController: _controller_pass,label_text: 'password',validat: 
+                      (value) {
+                     
+                       if(value!.isEmpty){
+                        return "Enter Password";
+                       }else if(value.length<6){
+                                              return "Password length should be more than 6 characters";
+        
+                       }else{
+                        return null;
+                       }
+                       
+                        
+        
+                      }
+                    ,str: " Enter Password")
+                    ],
+                  ),
+                  SizedBox(height: widthNHeight0(context, 0)*0.07,),
+                   Column(
+                    children: [
+                       Container(
+                      padding: const EdgeInsets.only(left: 30, right: 30),
+                      height: 60,
+                      child: InkWell(
+                        onTap: () {
+                          if (_formField.currentState!.validate()) {
+                            print("Success");
+                            emailController.clear();
+                            passController.clear();
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => const Home_page()));
+                          }
+                        },
+                        child: Container(
+                         // height: MediaQuery.of(context).size.width * .40,
+                        width: widthNHeight0(context, 0)*0.7, //////////////////edit the width
+                          decoration: BoxDecoration(
+                              color: const Color(0xff6482C4),
+                              borderRadius: BorderRadius.circular(5)),
+                          child: const Center(
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontFamily: 'Kadwa',
+                                  fontWeight: FontWeight.w600),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: Row(
+                    Container(
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Forgot Password",
+                                style: TextStyle(
+                                    color: Colors.grey[600], fontFamily: 'Kadwa'),
+                              ))
+                        ],
+                      ),
+                    ),
+                    ],
+                   ),
+                    Container(
+                      height: MediaQuery.of(context).size.width * .20,
+                    ),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        Text(
+                          "Not a member?",
+                          style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width * .033,
+                              fontFamily: 'Kadwa',
+                              color: Colors.grey[600]),
+                        ),
                         TextButton(
                             onPressed: () {},
                             child: Text(
-                              "Forgot Password",
+                              "Sign Up",
                               style: TextStyle(
-                                  color: Colors.grey[600], fontFamily: 'Kadwa'),
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * .033,
+                                  fontFamily: 'Kadwa',
+                                  color: Colors.grey[700]),
                             ))
                       ],
                     ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.width * .40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Not a member?",
-                        style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width * .033,
-                            fontFamily: 'Kadwa',
-                            color: Colors.grey[600]),
-                      ),
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width * .033,
-                                fontFamily: 'Kadwa',
-                                color: Colors.grey[700]),
-                          ))
-                    ],
-                  ),
-                ])),
+                  ])),
+        ),
       ),
     );
   }
