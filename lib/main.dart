037359dadc917fd6ae23a8cb9fca43c61e6c010f
Alaa  Jamal Abdalqader <93215532+eylem2002@users.noreply.github.com>
2023-12-17 12:21:49 +0300

    Connect To FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:new_batic/firebase_options.dart';
 import 'package:new_batic/view/screen/Main%20Screen/Profile%20Pages/Sell%20and%20rent/both/addPlanfloor.dart';
 import 'package:new_batic/view/screen/Main%20Screen/Search%20Page/Search_home_folder/Search_main.dart';
 import 'package:new_batic/core/services/EnterSevices.dart';
@@ -7,10 +8,15 @@ import 'package:new_batic/view/screen/Main%20Screen/Search%20Page/filters_screen
 import 'package:new_batic/view/screen/Sign%20in/up/splash_screen.dart';
 import 'package:new_batic/view/widget/BottomNavBar.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
 
 Future<void> main() async {
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
